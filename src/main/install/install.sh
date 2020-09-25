f4945a6fd7df1b2064f54ac24f90fb970bd2ef60
zemiak <zemiak@gmail.com>
2020-09-25 21:22:26 +0200

    no thumbnails generator

--- src/main/install/install.sh
+++ src/main/install/install.sh
@@ -2,6 +2,3 @@
 
 cp movies-update /etc/cron.daily/ || exit 30
 chmod +x /etc/cron.daily/movies-update
-
-cp ../cron-moviethumbnails/movies-moviethumbnails /etc/cron.daily/ || exit 40
-chmod +x /etc/cron.daily/movies-moviethumbnails
