20eae85d76117cf73ba222624ca398f2756861d4
zemiak <zemiak@gmail.com>
2020-09-25 21:47:26 +0200

    install/release

--- src/main/install/install.sh
+++ src/main/install/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+mkdir -p /usr/local/bin
+curl -L https://github.com/zemiak/movies-update/releases/download/latest/movies-update-runner.jar >/usr/local/bin/movies-update-runner.jar || exit 10
 cp movies-update /etc/cron.daily/ || exit 30
 chmod +x /etc/cron.daily/movies-update
